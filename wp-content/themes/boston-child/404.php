fed4ee40644a2784b3538ee77cc03a5df1b70d5a
Benjamin <benjamin.scheinhardt@gmail.com>
2017-12-28 16:51:24 +0100

    Cleanup de la page 404

--- wp-content/themes/boston-child/404.php
+++ wp-content/themes/boston-child/404.php
@@ -18,40 +18,14 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'boston' ); ?></p>
+					<p><?php 
+					printf( esc_html__('Il semble que rien n\'ait été trouvé à cet emplacement. Essayez peut-être une recherche ou retourner sur la page d\'%s ?', 'boston'),
+						sprintf('<a href="%s" class="link-theme">%s</a>',
+							esc_url( home_url( '/' )),
+							esc_html__( 'accueil', 'boston' ))
+				 	);
 
-					<?php
-						get_search_form();
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( boston_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'boston' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'boston' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 				</div><!-- .page-content -->
@@ -60,5 +34,7 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+
 <?php
+get_sidebar();
 get_footer();
